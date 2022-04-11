4dcf2e5f22c787991dbcaa75fdb8dbe180bdf460
danielrockhurst <danielneugent@amdg.rockhursths.edu>
2022-04-11 13:17:29 -0500

    testing with user disconnecting and connecting

--- index.js
+++ index.js
@@ -8,10 +8,39 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  // this is waiting for the message to send.
   socket.on('chat message', msg => {
+    // this is what displays the message on the screen
     io.emit('chat message', msg);
+    
   });
 });
+
+// this is showing whether or not someone is connected or disconnected
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  io.emit('chat message', 'User has entered the chat');
+  socket.on('disconnect', () => {
+    io.emit('chat message', 'A user has left the chat');
+    console.log('user disconnected');
+  });
+});
+
+io.sockets.on('connection', function (socket) {
+  var address = socket.handshake.address;
+  console.log('New connection from ' + address.address + ':' + address.port);
+});
+
+// this is saying on a connection to the socket
+io.on('connection', (socket) => {
+  // listening for a chat message 
+  socket.on('chat message', (msg) => {
+    // this is logging it to console
+    console.log('message: ' + msg);
+  });
+});
+
+
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
